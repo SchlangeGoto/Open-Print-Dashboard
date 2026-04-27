658d181b830da979d2f7a46496cd28acb04d27ad
SchlangeGoto <165955818+SchlangeGoto@users.noreply.github.com>
2026-04-27 20:54:39 +0200

    Update config.py

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -9,10 +9,5 @@ class Config:
     printer_serial: str = os.getenv("PRINTER_SERIAL", "")
     printer_access_code: str = os.getenv("PRINTER_ACCESS_CODE", "")
     database_url: str = os.getenv("DATABASE_URL", "")
-    cors_origins: List[str] = field(
-        default_factory=lambda: os.getenv(
-            "CORS_ORIGINS", "http://localhost:3000"
-        ).split(",")
-    )
 
 config = Config()
